fb9fb318c549dc669fff5ebe3046ed4bd3b7ce56
bytebyte6 <akahxx@qq.com>
2021-02-03 17:01:35 +0800

    整理依赖

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -25,22 +25,25 @@ object Libs {
     const val EXOPLAYER_DASH = "com.google.android.exoplayer:exoplayer-dash:${Versions.exoplayer}"
     const val EXOPLAYER_UI = "com.google.android.exoplayer:exoplayer-ui:${Versions.exoplayer}"
 
-    const val LEAK_CANARY = "com.squareup.leakcanary:leakcanary-android:2.6"
-    const val CRASH_REPORTER = "com.balsikandar.android:crashreporter:1.1.0"
+    //androidx
     const val ANDROIDX_CORE_KTX = "androidx.core:core-ktx:1.3.2"
     const val APPCOMPAT = "androidx.appcompat:appcompat:1.2.0"
-    const val MATERIAL_DESIGN = "com.google.android.material:material:1.3.0-beta01"
     const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.4"
     const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.2.0-alpha05"
     const val SWIPEREDRESHLAYOUT = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
-    const val GSON = "com.google.code.gson:gson:2.8.6"
-    const val LOTTIE = "com.airbnb.android:lottie:3.4.1"
-    const val KOTLIN_COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1"
     const val LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
     const val ACTIVITY = "androidx.activity:activity:1.2.0-alpha07"
     const val FRAGMENT = "androidx.fragment:fragment:1.3.0-alpha07"
     const val WORK_RUNTIME_KTX = "androidx.work:work-runtime-ktx:2.4.0"
     const val RECYCLERVIEW_SELECTION = "androidx.recyclerview:recyclerview-selection:1.0.0"
+    const val LIFECYCLE_COMMON =
+        "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle_version}"
+
+    const val LEAK_CANARY = "com.squareup.leakcanary:leakcanary-android:2.6"
+    const val CRASH_REPORTER = "com.balsikandar.android:crashreporter:1.1.0"
+    const val MATERIAL_DESIGN = "com.google.android.material:material:1.3.0-beta01"
+    const val GSON = "com.google.code.gson:gson:2.8.6"
+    const val LOTTIE = "com.airbnb.android:lottie:3.4.1"
     const val RXJAVA3 = "io.reactivex.rxjava3:rxjava:${Versions.rxjava}"
 
     //retrofit
@@ -54,14 +57,16 @@ object Libs {
     const val KOIN_SCPOE = "org.koin:koin-android-scope:${Versions.koin}"
     const val KOIIN_VIEWMODEL = "org.koin:koin-android-viewmodel:${Versions.koin}"
 
+    //glide
     const val GLIDE = "com.github.bumptech.glide:glide:${Versions.glide}"
 
     //room
     const val ROOM_RUNTIME = "androidx.room:room-runtime:${Versions.room}"
     const val ROOM_KTX = "androidx.room:room-ktx:${Versions.room}"
 
-    const val LIFECYCLE_COMMON =
-        "androidx.lifecycle:lifecycle-common-java8:${Versions.lifecycle_version}"
+    //kotlin
     const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val KOTLIN_COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1"
+
     const val GETIMAGE = "com.github.bytebyte6:GetImage:5.0"
 }
