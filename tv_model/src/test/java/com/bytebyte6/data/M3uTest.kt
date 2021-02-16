1eb1f17c678012b4d38ef8a03eede958a7165ae8
bytebyte6 <akahxx@qq.com>
2021-02-16 18:36:50 +0800

    删除测试文件

--- tv_model/src/test/java/com/bytebyte6/data/M3uTest.kt
+++ tv_model/src/test/java/com/bytebyte6/data/M3uTest.kt
@@ -5,8 +5,12 @@ import java.io.File
 
 class M3uTest {
     @Test
+    fun test1(){
+        println(File("..","pic").absolutePath)
+    }
+    @Test
     fun list() {
-        val modelDir = File(".", "channels")
+        val modelDir = File("..", "channels")
         val listFiles = modelDir.listFiles()
         listFiles?.forEach { file ->
             val tvs = M3u.getTvs(file)
