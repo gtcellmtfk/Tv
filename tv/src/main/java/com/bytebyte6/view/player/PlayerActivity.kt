d6c99d9dbd4788ae78822c01196d0f8cc7f2ca0e
bytebyte6 <akahxx@qq.com>
2021-02-20 13:59:19 +0800

    Add activity_player.xml Fix ParseM3uUseCase.kt

--- tv/src/main/java/com/bytebyte6/view/player/PlayerActivity.kt
+++ tv/src/main/java/com/bytebyte6/view/player/PlayerActivity.kt
@@ -8,7 +8,7 @@ import com.bytebyte6.view.replaceNotAddToBackStack
 class PlayerActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_player)
 
         if (savedInstanceState == null) {
             replaceNotAddToBackStack(
